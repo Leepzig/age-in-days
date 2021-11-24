84f56cf29cfaa69432115f35b5e3ffa08ae1b2e1
andrewsimmons <neon.duncan@gmail.com>
2021-11-23 21:18:22 -0600

    added input and button with testing in jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
+
+test('renders a button that says "Get Day Age"', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: "Get Day Age"})
+  expect(button).toBeInTheDocument()
+})
+
+test('renders an text input', () => {
+  render(<App />)
+  const input = screen.getByLabelText("birthday")
+  expect(input).toBeInTheDocument()
+})
+
+test('User is able to type in the birthday input', () => {
+  render(<App />)
+  const input = screen.getByLabelText("birthday")
+  userEvent.type(input, "07/19/2000")
+  expect(input).toHaveValue("07/19/2000")
+
+})
