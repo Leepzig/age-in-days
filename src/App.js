a72972478d8bbbb652e116f03387854233b47357
andrewsimmons <neon.duncan@gmail.com>
2021-11-28 09:10:17 -0600

    refactored the states in App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import { Button, TextField, Box, Grid } from '@mui/material';
+import { Button, TextField, Box, Grid, FormControl } from '@mui/material';
 import { useState } from 'react'
 import Errors from './components/Errors';
 import BirthdayList from './components/BirthdayList';
 
 function App() {
-  const [birthday, setBirthday] = useState("")
+  const [form, setForm] = useState("")
+  const [birthday, setBirthday] = useState(null)
+  const [errors, setErrors] = useState(null)
 
   const handleChange = (e) => {
-    setBirthday(e.target.value)
+    setForm(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(form)
+    setBirthday(form)
   }
   return (
     <Box sx={{ mt: 20, mx: "auto", width: 200 }}>
       <Grid>
-        <Errors />
-        <TextField id="outlined-basic" label="birthday" value={birthday} variant="outlined" onChange={handleChange}/>
-        <Button>Get Day Age</Button>
-
+        {errors ? <Errors /> : null}
+        <form onSubmit={handleSubmit}>
+          <TextField id="outlined-basic" label="birthday" value={form} variant="outlined" onChange={handleChange}/>
+          <Button onClick={handleSubmit}>Get Day Age</Button>
+        </form>
+        <BirthdayList birthday={birthday} />
       </Grid>
     </Box>
   );
--- src/components/BirthdayList.js
+++ src/components/BirthdayList.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
-const BirthdayList = () => {
+const BirthdayList = ( { birthday }) => {
+    if (!birthday) return null
+
+    const parseBirthday = birthday => {
+        const birthdayArray = birthday.split("/")
+        const day = new Date
+        day.setDate(birthdayArray[1])
+        day.setMonth(birthdayArray[0] - 1)
+        day.setFullYear(birthdayArray[2])
+        debugger
+        return day
+    }
+
+    parseBirthday("11/14/1992")
     return (
         <div>
             
